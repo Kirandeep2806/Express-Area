97b4accbe2186363b12f3bb8530d61b41bbb5897
Kiran Deep <kirandeep102030@gmail.com>
2022-10-25 06:00:04 +0530

    Added code to configure the routes

--- first-express-app/server.mjs
+++ first-express-app/server.mjs
@@ -4,9 +4,14 @@ const app = express()
 const port = 6996
 
 app.get("/", (req, res) => {
-    res.send("<h1>King!!</h1>")
+    res.send("<h1>I am on the Screen!!</h1>")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>Hey, I am Kiran Deep</h1>")
+    console.log(res.statusCode);
 })
 
 app.listen(port, () => {
-    console.log(`I am on the port ${port}`);
+    console.log(`Server running on the port ${port}`);
 })
