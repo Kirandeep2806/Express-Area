b49fde494b6418ca9c96657e2893292d6b08ab19
Kiran Deep <kirandeep102030@gmail.com>
2022-10-25 06:42:34 +0530

    body-parser is replaced with similar express method

--- first-express-app/AgeCalculator/ageCalculator.js
+++ first-express-app/AgeCalculator/ageCalculator.js
@@ -1,8 +1,7 @@
 const express = require("express")
-const bodyParser = require("body-parser")
 
 const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
