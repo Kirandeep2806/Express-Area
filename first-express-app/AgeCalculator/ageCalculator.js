ead925a3fe3b1dd6be5f6c366904a2cfadac9692
Kiran Deep <kirandeep102030@gmail.com>
2022-10-25 06:36:35 +0530

    Usage of body-parser to handle post request(process data)

--- first-express-app/AgeCalculator/ageCalculator.js
+++ first-express-app/AgeCalculator/ageCalculator.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
-}).listen(6969)
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+})
+
+app.listen(6969, () => {
+    console.log("Serving on port 6969!!");
+})
