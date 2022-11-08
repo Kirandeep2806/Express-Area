36b085155de72b396d85053427421c33d031493f
Kiran Deep <kirandeep102030@gmail.com>
2022-11-08 18:16:40 +0530

    Added Weather API along with age calculator

--- first-express-app/AgeCalculator/ageCalculator.js
+++ first-express-app/AgeCalculator/ageCalculator.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const {AgeFromDateString, AgeFromDate} = require("age-calculator");
+const { AgeFromDateString, AgeFromDate } = require("age-calculator");
 const https = require("https");
+const { setTimeout } = require("timers/promises");
 
 const app = express()
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
 app.get("/weather", (req, res) => {
+    console.log(res.statusCode);
     res.sendFile(__dirname + "/getLocation.html")
 })
 
@@ -18,25 +20,37 @@ app.post("/weather", (req, res) => {
 
     // Using the API for fetching the location coordinates
     const data = req.body;
-    const geoApiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${data.location}&appid=${apiKey}`;
+    const location = data.location
+    const geoApiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${apiKey}`;
     https.get(geoApiURL, (HttpsResponse) => {
         HttpsResponse.on('data', (data) => {
             const JSONResponse = JSON.parse(data)
-            let latitude = JSONResponse[0].lat
-            let longitude = JSONResponse[0].lon
+            if (JSONResponse.length>0) {
+                let latitude = JSONResponse[0].lat
+                let longitude = JSONResponse[0].lon
 
-            // Using the API to fetch the weather
-            const language = "en";
-            const weatherApiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=${language}`;
-            https.get(weatherApiURL, (HttpsResponse) => {
-                HttpsResponse.on("data", (data) => {
-                    const JSONResponse = JSON.parse(data);
-                    console.log(JSONResponse);
+                // Using the API to fetch the weather
+                const language = "en";
+                const weatherApiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=${language}`;
+                https.get(weatherApiURL, (HttpsResponse) => {
+                    HttpsResponse.on("data", (data) => {
+                        const JSONResponse = JSON.parse(data);
+                        res.write(`<p><h2>The Weather report @<b>${location}</b> is:</h2><br/>`)
+                        res.write(`Temperature : ${JSONResponse.main.temp} Celsius<br/>`)
+                        res.write(`Humidity : ${JSONResponse.main.humidity}%<br/>`)
+                        res.write(`Wind Speed : ${JSONResponse.wind.speed} km/h<br/>`)
+                        let desc = JSONResponse.weather[0].description
+                        res.write(`Description : ${desc.charAt(0).toUpperCase() + desc.slice(1)}<br/><br/>`)
+                        res.write("<a href='/weather'>Go To Home</a></p>")
+                        res.send()
+                    })
                 })
-            })
+            }
+            else {
+                res.redirect("/weather");
+            }
         })
     })
-
 })
 
 app.post("/", (req, res) => {
